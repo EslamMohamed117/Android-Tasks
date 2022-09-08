73c827d2d0b7e3b5b2b9cc09aac9c0af630829f9
xS4yk0x <sayko117@gmail.com>
2022-09-09 00:12:16 +0200

    SQLite DB ADDED

--- app/src/main/java/com/example/task1/database/DatabaseContract.java
+++ app/src/main/java/com/example/task1/database/DatabaseContract.java
@@ -1,5 +1,26 @@
 package com.example.task1.database;
 
+import android.provider.BaseColumns;
+
 public final class DatabaseContract {
     private DatabaseContract(){}
+    public static final class ProductInfo implements BaseColumns{
+        public static final String TABLE_NAME = "product_info";
+        public static final String PRODUCT_ID = "product_id";
+        public static final String PRODUCT_NAME = "product_name";
+        public static final String PRODUCT_PRICE = "product_price";
+        public static final String PRODUCT_DESCRIPTION = "product_description";
+        public static final String PRODUCT_COUNT = "product_count";
+        public static final String PRODUCT_PICTURE = "product_picture";
+        public static final String SQL_CREATE_TABLE =
+                "CREATE TABLE "+TABLE_NAME+ "(" +
+                _ID+" INTEGER PRIMARY KEY, " +
+                PRODUCT_ID + " INTEGER UNIQUE NOT NULL, " +
+                PRODUCT_NAME + " TEXT NOT NULL, " +
+                PRODUCT_PRICE + " REAL NOT NULL, " +
+                PRODUCT_DESCRIPTION + " TEXT, " +
+                PRODUCT_COUNT + " INTEGER NOT NULL," +
+                PRODUCT_PICTURE + " INTEGER NOT NULL" +
+                        ")";
+    }
 }
---
+++ app/src/main/java/com/example/task1/database/DatabaseOpenHelper.java
@@ -0,0 +1,50 @@
+package com.example.task1.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.task1.Product;
+import com.example.task1.ProductsList;
+import com.example.task1.database.DatabaseContract.ProductInfo;
+
+import java.util.ArrayList;
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "Online_Store.db";
+    public static final int DB_VERSION = 1;
+    public DatabaseOpenHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ProductInfo.SQL_CREATE_TABLE);
+        insertProducts(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    private void insertProducts(SQLiteDatabase db)
+    {
+        ArrayList<Product> list = ProductsList.getInstance().getList();
+        for(Product product : list)
+        {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ProductInfo.PRODUCT_ID,product.getProductID());
+            contentValues.put(ProductInfo.PRODUCT_NAME,product.getProductName());
+            contentValues.put(ProductInfo.PRODUCT_PRICE,product.getProductPrice());
+            contentValues.put(ProductInfo.PRODUCT_DESCRIPTION,product.getProductDescription());
+            contentValues.put(ProductInfo.PRODUCT_COUNT,product.getProductCount());
+            contentValues.put(ProductInfo.PRODUCT_PICTURE,product.getProductPicture());
+            db.insert(ProductInfo.TABLE_NAME,null, contentValues);
+        }
+
+
+    }
+}
