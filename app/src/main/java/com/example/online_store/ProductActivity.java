9d64b7364c46b31aa98c0c7592d26eee5889fef4
xS4yk0x <sayko117@gmail.com>
2022-09-09 15:25:58 +0200

    Product Info is now being loaded from DB instead of ProductList

--- app/src/main/java/com/example/online_store/ProductActivity.java
+++ app/src/main/java/com/example/online_store/ProductActivity.java
@@ -4,24 +4,36 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.online_store.database.DatabaseContract;
+import com.example.online_store.database.DatabaseContract.ProductInfo;
+import com.example.online_store.database.DatabaseOpenHelper;
+
 public class ProductActivity extends AppCompatActivity {
+
+    private DatabaseOpenHelper mOpenHelper;
+    private int mItemPosition;
+    private String productName;
+    private String productDescription;
+    private float productPrice;
+    private int productID;
+    private int productCount;
+    private int productPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_gallery);
+        mOpenHelper = new DatabaseOpenHelper(this);
         Intent intent = getIntent();
-        int itemPosition = intent.getIntExtra(RecyclerAdapter.ITEMPOSITIONTAG,0);
-        Product product= ProductsList.getInstance().getList().get(itemPosition);
-        ((TextView) findViewById(R.id.product_profile_name)).setText(product.getProductName());
-        ((TextView) findViewById(R.id.product_profile_desc)).setText(product.getProductDescription());
-        ((TextView) findViewById(R.id.product_profile_count)).setText("Available: "+Integer.toString(product.getProductCount()));
-        ((TextView) findViewById(R.id.product_profile_price)).setText("Price: "+ Float.toString(product.getProductPrice())+"$");
-        ((ImageView) findViewById(R.id.product_profile_picture_nav)).setImageResource(product.getProductPicture());
+        mItemPosition = intent.getIntExtra(RecyclerAdapter.ITEMPOSITIONTAG,0);
+        loadProductInfoFromDB();
 
 
         ActionBar actionBar;
@@ -31,5 +43,54 @@ public class ProductActivity extends AppCompatActivity {
         actionBar.setTitle("Product Info");
 
         Utils.initializeActivity(this);
+    }
+    private void loadProductInfoFromDB()
+    {
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        String selection = ProductInfo.PRODUCT_ID+"= ?";
+        String[] selectionArgs = {Integer.toString(mItemPosition)};
+
+        final String[] columns = {
+                ProductInfo.PRODUCT_ID,
+                ProductInfo.PRODUCT_NAME,
+                ProductInfo.PRODUCT_PRICE,
+                ProductInfo.PRODUCT_DESCRIPTION,
+                ProductInfo.PRODUCT_COUNT,
+                ProductInfo.PRODUCT_PICTURE};
+
+        Cursor cursor = db.query(ProductInfo.TABLE_NAME, columns, selection, selectionArgs, null, null, null);
+
+        int productIDPos = cursor.getColumnIndex(ProductInfo.PRODUCT_ID);
+        int productNamePos = cursor.getColumnIndex(ProductInfo.PRODUCT_NAME);
+        int productPricePos = cursor.getColumnIndex(ProductInfo.PRODUCT_PRICE);
+        int productDescriptionPos = cursor.getColumnIndex(ProductInfo.PRODUCT_DESCRIPTION);
+        int productCountPos = cursor.getColumnIndex(ProductInfo.PRODUCT_COUNT);
+        int productPicturePos = cursor.getColumnIndex(ProductInfo.PRODUCT_PICTURE);
+
+        while(cursor.moveToNext())
+        {
+            productID=cursor.getInt(productIDPos);
+            productName=cursor.getString(productNamePos);
+            productDescription= cursor.getString(productDescriptionPos);
+            productPrice=cursor.getFloat(productPricePos);
+            productCount=cursor.getInt(productCountPos);
+            productPicture=cursor.getInt(productPicturePos);
+        }
+        cursor.close();
+        displayProduct();
+    }
+    private void displayProduct()
+    {
+        ((TextView) findViewById(R.id.product_profile_name)).setText(productName);
+        ((TextView) findViewById(R.id.product_profile_desc)).setText(productDescription);
+        ((TextView) findViewById(R.id.product_profile_count)).setText("Available: "+productCount);
+        ((TextView) findViewById(R.id.product_profile_price)).setText("Price: "+ productPrice+"$");
+        ((ImageView) findViewById(R.id.product_profile_picture_nav)).setImageResource(productPicture);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mOpenHelper.close();
+        super.onDestroy();
     }
 }
