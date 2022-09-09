aa86d834007df212ba7ed776bde7c8cc30333c6d
xS4yk0x <sayko117@gmail.com>
2022-09-09 20:51:23 +0200

    Adapting the RecyclerAdapter to load from the DB instead of the ProductsList

--- app/src/main/java/com/example/online_store/RecyclerAdapter.java
+++ app/src/main/java/com/example/online_store/RecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.online_store;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,18 +12,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.online_store.database.DatabaseContract;
+import com.example.online_store.database.DatabaseContract.ProductInfo;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
 
     private final Context mContext;
     private final LayoutInflater mLayoutInflater;
-    private final ProductsList mList;
+    //private final ProductsList mList;
     public static final String ITEMPOSITIONTAG = "RITEMPOSITIONTAG";
+    private Cursor mCursor;
+    private int mProductIDPos;
+    private int mProductNamePos;
+    private int mProductPricePos;
+    private int mProductDescriptionPos;
+    private int mProductCountPos;
+    private int mProductPicturePos;
 
 
-    public RecyclerAdapter(Context context) {
+    public RecyclerAdapter(Context context, Cursor cursor) {
         mContext = context;
         mLayoutInflater = LayoutInflater.from(mContext);
-        mList= ProductsList.getInstance();
+        //mList= ProductsList.getInstance();
+        mCursor = cursor;
+        populateColumnsPosition();
+    }
+
+    private void populateColumnsPosition() {
+        if(mCursor == null)
+            return;
+        mProductIDPos = mCursor.getColumnIndex(ProductInfo.PRODUCT_ID);
+        mProductNamePos = mCursor.getColumnIndex(ProductInfo.PRODUCT_NAME);
+        mProductPricePos = mCursor.getColumnIndex(ProductInfo.PRODUCT_PRICE);
+        mProductDescriptionPos = mCursor.getColumnIndex(ProductInfo.PRODUCT_DESCRIPTION);
+        mProductCountPos = mCursor.getColumnIndex(ProductInfo.PRODUCT_COUNT);
+        mProductPicturePos = mCursor.getColumnIndex(ProductInfo.PRODUCT_PICTURE);
+    }
+    public void changeCursor(Cursor cursor)
+    {
+        if(mCursor != null)
+            mCursor.close();
+        mCursor = cursor;
+        populateColumnsPosition();
+        notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
@@ -53,14 +85,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.mProductName.setText(mList.getList().get(position).getProductName());
-        holder.mProductDescription.setText(mList.getList().get(position).getProductDescription());
-        holder.mPicture.setImageResource(mList.getList().get(position).getProductPicture());
+        mCursor.moveToPosition(position);
+        holder.mProductName.setText(mCursor.getString(mProductNamePos));
+        holder.mProductDescription.setText(mCursor.getString(mProductDescriptionPos));
+        holder.mPicture.setImageResource(mCursor.getInt(mProductPicturePos));
         holder.mCurrentPosition=position;
     }
 
     @Override
     public int getItemCount() {
-        return mList.getList().size();
+        return mCursor==null ? 0 : mCursor.getCount();
     }
 }
--- app/src/main/java/com/example/online_store/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/online_store/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.online_store.ui.home;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,36 +13,48 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.online_store.RecyclerAdapter;
+import com.example.online_store.database.DatabaseContract;
+import com.example.online_store.database.DatabaseContract.ProductInfo;
+import com.example.online_store.database.DatabaseOpenHelper;
 import com.example.online_store.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private RecyclerAdapter adapter;
+    private DatabaseOpenHelper mOpenHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
+        mOpenHelper = new DatabaseOpenHelper(getContext());
         final RecyclerView mList = binding.listItems;
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         mList.setLayoutManager(layoutManager);
-        adapter = new RecyclerAdapter(getContext());
+        adapter = new RecyclerAdapter(getContext(), null);
         mList.setAdapter(adapter);
         return root;
     }
 
     @Override
     public void onResume() {
+        loadProducts();
         super.onResume();
-        adapter.notifyDataSetChanged();
+    }
+
+    private void loadProducts() {
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final String[] columns = {ProductInfo.PRODUCT_NAME, ProductInfo.PRODUCT_DESCRIPTION, ProductInfo.PRODUCT_PICTURE};
+        Cursor cursor = db.query(ProductInfo.TABLE_NAME, columns, null, null, null, null, null);
+        adapter.changeCursor(cursor);
     }
 
     @Override
     public void onDestroyView() {
-        super.onDestroyView();
         binding = null;
+        mOpenHelper.close();
+        super.onDestroyView();
     }
 }
